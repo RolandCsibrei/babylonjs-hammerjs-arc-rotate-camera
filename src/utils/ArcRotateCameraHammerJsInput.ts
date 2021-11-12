5cf59bd728c09ef92e7a1704401b6b5451ae58ca
Roland Csibrei <roland.csibrei@nascor.tech>
2021-11-12 13:33:31 +0100

    Debug gadgets

--- src/utils/ArcRotateCameraHammerJsInput.ts
+++ src/utils/ArcRotateCameraHammerJsInput.ts
@@ -5,10 +5,24 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { ArcRotateCamera, Camera, CameraInputTypes, Color3, FreeCamera, ICameraInput, MeshBuilder, StandardMaterial, Vector2, Vector3 } from '@babylonjs/core'
-
+import {
+  ArcRotateCamera,
+  CameraInputTypes,
+  Color3,
+  Color4,
+  FreeCamera,
+  ICameraInput,
+  LinesMesh,
+  Mesh,
+  MeshBuilder,
+  StandardMaterial,
+  Vector2,
+  Vector3
+} from '@babylonjs/core'
+import { AdvancedDynamicTexture, Button } from '@babylonjs/gui'
+import * as GUI from '@babylonjs/gui'
 import 'hammerjs'
-import { screenToWorld, worldToScreen } from './babylonjs'
+import { worldToScreen } from './babylonjs'
 
 interface DoubleTouchInfo {
   center: Vector2
@@ -122,61 +136,151 @@ export class ArcRotateCameraHammerJsInput implements ICameraInput<ArcRotateCamer
         scene.activeCameras.push(scene.activeCamera)
       }
 
+      const adt = AdvancedDynamicTexture.CreateFullscreenUI('debug', true, scene)
+
+      const debugLinePoints = [new Vector3(0, 0, 0), new Vector3(40, 10, 0)]
+      const debugLinePointsColors = [new Color4(1, 0, 0, 1), new Color4(0, 0, 1, 1)]
+
+      const debugLineOptions: {
+        points: Vector3[]
+        colors: Color4[]
+        updatable?: boolean
+        instance?: LinesMesh
+      } = {
+        points: debugLinePoints,
+        updatable: true,
+        colors: debugLinePointsColors
+      }
+
       const secondCamera = new FreeCamera('debugCamera', new Vector3(0, 0, -30), scene)
       secondCamera.layerMask = 0x20000000
       scene.activeCameras.push(secondCamera)
 
+      let lines = MeshBuilder.CreateLines('debugLines', debugLineOptions, scene)
+      lines.layerMask = 0x20000000
+
       this._debugCamera = new ArcRotateCamera('debugCamera', 0, 0, 10, Vector3.Zero(), scene)
       this._debugCamera.layerMask = 0x20000000
 
-      const size = 1
-      const centerMarker = MeshBuilder.CreateBox('centerMarker', { size: 0.5 }, scene)
+      const diameter = 1
+      const centerMarker = MeshBuilder.CreateSphere('centerMarker', { diameter: 0.5 }, scene)
       const centerMarkerMaterial = new StandardMaterial('centerMarkerMaterial', scene)
-      centerMarkerMaterial.emissiveColor = Color3.Green()
+      centerMarkerMaterial.emissiveColor = Color3.Gray()
+      centerMarkerMaterial.disableLighting = true
       centerMarker.material = centerMarkerMaterial
       centerMarker.layerMask = 0x20000000
+      centerMarker.visibility = 0.4
+      const centerButton = this._addDebugButton(0, adt)
 
-      const touch1Marker = MeshBuilder.CreateBox('touch1marker', { size }, scene)
+      const touch1Marker = MeshBuilder.CreateSphere('touch1marker', { diameter }, scene)
       const touch1MarkerMaterial = new StandardMaterial('touch1MarkerMaterial', scene)
       touch1MarkerMaterial.emissiveColor = Color3.Red()
+      touch1MarkerMaterial.disableLighting = true
       touch1Marker.material = touch1MarkerMaterial
       touch1Marker.layerMask = 0x20000000
+      touch1Marker.visibility = 0.6
+      const touch1Button = this._addDebugButton(1, adt)
 
-      const touch2Marker = MeshBuilder.CreateBox('touch2marker', { size }, scene)
+      const touch2Marker = MeshBuilder.CreateSphere('touch2marker', { diameter }, scene)
       const touch2MarkerMaterial = new StandardMaterial('touch2MarkerMaterial', scene)
       touch2MarkerMaterial.emissiveColor = Color3.Blue()
+      touch2MarkerMaterial.disableLighting = true
       touch2Marker.material = touch2MarkerMaterial
       touch2Marker.layerMask = 0x20000000
+      touch2Marker.visibility = 0.6
+      const touch2Button = this._addDebugButton(2, adt)
 
       const mapValue = (value: number, x1: number, y1: number, x2: number, y2: number) => ((value - x1) * (y2 - x2)) / (y1 - x1) + x2
       const renderWidth = engine.getRenderWidth()
       const renderHeight = engine.getRenderHeight()
+      // let angleMarker: Mesh | null = null
+
+      // const angleMarkerMaterial = new StandardMaterial('angleMarkerMaterial', scene)
+      // angleMarkerMaterial.emissiveColor = Color3.Yellow()
+      // angleMarkerMaterial.alpha = 0.1
+
       scene.onBeforeRenderObservable.add(() => {
-        // DEBUG MARKERS
-        const rw = 22
-        const rh = 12
+        if (this._debugCamera) {
+          // DEBUG MARKERS
+          const rw = 22
+          const rh = 12
 
-        let x = mapValue(this._oldPointer0.x, 0, renderWidth, -rw, rw)
-        let y = mapValue(this._oldPointer0.y, 0, renderHeight, rh, -rh)
-        touch1Marker.position.x = x
-        touch1Marker.position.y = y
-        touch1Marker.position.z = 0
+          // if (angleMarker) {
+          //   angleMarker.dispose()
+          // }
+          // angleMarker = MeshBuilder.CreateDisc(
+          //   'angleMarker',
+          //   { radius: this._oldInfo.distance / 100, tessellation: 180, arc: (Math.PI * 2) / this._oldInfo.angle },
+          //   scene
+          // )
+          // angleMarker.position = centerMarker.position
+          // angleMarker.material = angleMarkerMaterial
+          // angleMarker.layerMask = 0x20000000
 
-        x = mapValue(this._oldPointer1.x, 0, renderWidth, -rw, rw)
-        y = mapValue(this._oldPointer1.y, 0, renderHeight, rh, -rh)
-        touch2Marker.position.x = x
-        touch2Marker.position.y = y
-        touch2Marker.position.z = 0
+          if (touch1Button.textBlock) {
+            touch1Button.textBlock.text = `[${this._oldPointer0.x}, ${this._oldPointer0.y}]`
+          }
+          touch1Button.leftInPixels = this._oldPointer0.x - renderWidth / 2
+          touch1Button.topInPixels = this._oldPointer0.y - renderHeight / 2 - 50
 
-        x = mapValue(this._oldInfo.center.x, 0, renderWidth, -rw, rw)
-        y = mapValue(this._oldInfo.center.y, 0, renderHeight, rh, -rh)
-        centerMarker.position.x = x
-        centerMarker.position.y = y
-        centerMarker.position.z = 0
+          if (touch2Button.textBlock) {
+            touch2Button.textBlock.text = `[${this._oldPointer1.x}, ${this._oldPointer1.y}]`
+          }
+          touch2Button.leftInPixels = this._oldPointer1.x - renderWidth / 2
+          touch2Button.topInPixels = this._oldPointer1.y - renderHeight / 2 - 60
+
+          if (centerButton.textBlock) {
+            centerButton.textBlock.text = `[${this._oldInfo.center.x}, ${this._oldInfo.center.y}] (${this._oldInfo.deltaAngle.toPrecision(4)} rad)`
+          }
+          centerButton.leftInPixels = this._oldInfo.center.x - renderWidth / 2
+          centerButton.topInPixels = this._oldInfo.center.y - renderHeight / 2 - 60
+
+          let x = mapValue(this._oldPointer0.x, 0, renderWidth, -rw, rw)
+          let y = mapValue(this._oldPointer0.y, 0, renderHeight, rh, -rh)
+          touch1Marker.position.x = x
+          touch1Marker.position.y = y
+          touch1Marker.position.z = 0
+
+          x = mapValue(this._oldPointer1.x, 0, renderWidth, -rw, rw)
+          y = mapValue(this._oldPointer1.y, 0, renderHeight, rh, -rh)
+          touch2Marker.position.x = x
+          touch2Marker.position.y = y
+          touch2Marker.position.z = 0
+
+          if (this._oldInfo.distance < 1) {
+            touch2Marker.isVisible = false
+            touch2Button.isVisible = false
+
+            centerMarker.isVisible = false
+            centerButton.isVisible = false
+          } else {
+            touch2Marker.isVisible = true
+            touch2Button.isVisible = true
+
+            centerMarker.isVisible = true
+            centerButton.isVisible = true
+          }
+          x = mapValue(this._oldInfo.center.x, 0, renderWidth, -rw, rw)
+          y = mapValue(this._oldInfo.center.y, 0, renderHeight, rh, -rh)
+          centerMarker.position.x = x
+          centerMarker.position.y = y
+          centerMarker.position.z = 0
+
+          debugLineOptions.points[0].x = touch1Marker.position.x
+          debugLineOptions.points[0].y = touch1Marker.position.y
+          debugLineOptions.points[1].x = touch2Marker.position.x
+          debugLineOptions.points[1].y = touch2Marker.position.y
+
+          debugLineOptions.instance = lines
+
+          lines = MeshBuilder.CreateLines('debugLines', debugLineOptions)
+          lines.layerMask = 0x20000000
+        }
       })
     }
 
     // DEBUG markers
+
     const size = 1
     const centerMarker = MeshBuilder.CreateBox('centerMarker', { size: 0.5 }, scene)
     const centerMarkerMaterial = new StandardMaterial('centerMarkerMaterial', scene)
@@ -216,7 +320,12 @@ export class ArcRotateCameraHammerJsInput implements ICameraInput<ArcRotateCamer
       if (!isRotating || !isBetaPanning) {
         // console.log('panstart')
         this._oldPointer0 = { x: e.pointers[0].clientX, y: e.pointers[0].clientY, deltaCenter: new Vector2(), distance: 0, deltaDistance: 0 }
+        this._oldPointer1 = { x: e.pointers[0].clientX, y: e.pointers[0].clientY, deltaCenter: new Vector2(), distance: 0, deltaDistance: 0 }
         this._startPointer0 = { x: e.pointers[0].clientX, y: e.pointers[0].clientY, deltaCenter: new Vector2(), distance: 0, deltaDistance: 0 }
+        this._startPointer1 = { x: e.pointers[0].clientX, y: e.pointers[0].clientY, deltaCenter: new Vector2(), distance: 0, deltaDistance: 0 }
+        const info = getCenterAngleDistance(this._startPointer0, this._startPointer1)
+        this._oldInfo = { ...info }
+        this._startInfo = { ...info }
 
         isPanning = true
         startCenterX = e.pointers[0].clientX
@@ -232,6 +341,12 @@ export class ArcRotateCameraHammerJsInput implements ICameraInput<ArcRotateCamer
         const dx = -(startCenterX - e.pointers[0].clientX) * this.xPanningRatio
         const dy = (startCenterY - e.pointers[0].clientY) * this.zPanningRatio
         panMove(dx, dy)
+
+        this._oldPointer0.x = e.pointers[0].clientX
+        this._oldPointer0.y = e.pointers[0].clientY
+
+        this._oldPointer1.x = e.pointers[0].clientX
+        this._oldPointer1.y = e.pointers[0].clientY
       }
     })
 
@@ -251,6 +366,9 @@ export class ArcRotateCameraHammerJsInput implements ICameraInput<ArcRotateCamer
     })
 
     manager.on('rotatestart', e => {
+      if (isRotating || isBetaPanning) {
+        return
+      }
       // console.log('rotatestart')
 
       isRotating = true
@@ -321,12 +439,12 @@ export class ArcRotateCameraHammerJsInput implements ICameraInput<ArcRotateCamer
 
       let addAngle = 0
       if (firstTouchLow) {
-        //   if (p0.y === p1.y && p0.x < p1.x) {
-        //     addAngle = Math.PI
-        //   }
-        //   if (p0.y === p1.y && p0.x > p1.x) {
-        //     addAngle = Math.PI
-        //   }
+        if (p0.y === p1.y && p0.x > p1.x) {
+          addAngle = -Math.PI
+        }
+        if (p0.y === p1.y && p0.x < p1.x) {
+          addAngle = Math.PI
+        }
 
         if (p0.y < p1.y && p0.x < p1.x) {
           addAngle = Math.PI
@@ -343,20 +461,6 @@ export class ArcRotateCameraHammerJsInput implements ICameraInput<ArcRotateCamer
         }
       }
 
-      // DEBUG MARKERS
-      // const s = 0.04
-      // touch1Marker.position = new Vector3(p0.x, 1, p0.y).scale(s)
-      // touch2Marker.position = new Vector3(p1.x, 1, p1.y).scale(s)
-      // centerMarker.position = new Vector3(info.center.x, 1, info.center.y).scale(s)
-
-      // touch1Marker.position.x -= 40
-      // touch2Marker.position.x -= 40
-      // centerMarker.position.x -= 40
-      // touch1Marker.position.z -= 40
-      // touch2Marker.position.z -= 40
-      // centerMarker.position.z -= 40
-      //
-
       const deltaAngle = info.deltaAngle + addAngle
 
       if (!isBetaPanning) {
@@ -364,34 +468,36 @@ export class ArcRotateCameraHammerJsInput implements ICameraInput<ArcRotateCamer
         panMove(info.deltaCenter.x / 20, info.deltaCenter.y / 20)
 
         this.camera.radius = startRadius + info.deltaDistance / 10
-
-        // console.log(
-        //   'startAlpha',
-        //   (startAlpha * 180) / Math.PI,
-        //   'startInfo.angle',
-        //   (startInfo.angle * 180) / Math.PI,
-        //   'angle',
-        //   (info.angle * 180) / Math.PI,
-        //   'addAngle',
-        //   (addAngle * 180) / Math.PI,
-        //   'deltaAngle',
-        //   (deltaAngle * 180) / Math.PI,
-        //   'info.deltaCenter.x',
-        //   info.deltaCenter.x,
-        //   'info.deltaCenter.y',
-        //   info.deltaCenter.y,
-        //   'p0',
-        //   p0.x,
-        //   p0.y,
-        //   'p1',
-        //   p1.x,
-        //   p1.y,
-        //   'firstTouchLow',
-        //   firstTouchLow
-        // )
       } else {
         this.camera.beta = startBeta + info.deltaCenter.y / 400
       }
+
+      // console.log(
+      //   'startAlpha',
+      //   (startAlpha * 180) / Math.PI,
+      //   'startInfo.angle',
+      //   (this._startInfo.angle * 180) / Math.PI,
+      //   'angle',
+      //   (info.angle * 180) / Math.PI,
+      //   'addAngle',
+      //   (addAngle * 180) / Math.PI,
+      //   'deltaAngle',
+      //   (deltaAngle * 180) / Math.PI,
+      //   'info.deltaCenter.x',
+      //   info.deltaCenter.x,
+      //   'info.deltaCenter.y',
+      //   info.deltaCenter.y,
+      //   'p0',
+      //   p0.x,
+      //   p0.y,
+      //   'p1',
+      //   p1.x,
+      //   p1.y,
+      //   'firstTouchLow',
+      //   firstTouchLow,
+      //   'distance',
+      //   info.distance
+      // )
 
       this._oldPointer0.x = e.pointers[0].clientX
       this._oldPointer0.y = e.pointers[0].clientY
@@ -504,6 +610,39 @@ export class ArcRotateCameraHammerJsInput implements ICameraInput<ArcRotateCamer
 
   public checkInputs() {
     // this.camera.inertialRadiusOffset = -0.1
+  }
+
+  private _addDebugButton(i: number, adt: AdvancedDynamicTexture) {
+    const btn = Button.CreateSimpleButton(`btn${i}`, `button-${i}`)
+    btn.widthInPixels = 80
+    btn.heightInPixels = 32
+
+    btn.color = '#fff'
+    btn.fontWeight = '300'
+    btn.fontSizeInPixels = 10
+    btn.background = '#000'
+    btn.alpha = 0.9
+    btn.cornerRadius = 4
+    btn.thickness = 0
+    btn.alpha = 0.7
+
+    // this._buttons.push(btn)
+
+    // const line = new GUI.Line()
+    // line.name = `line_${i}`
+    // line.lineWidth = 4
+    // line.color = '#444'
+    // line.connectedControl = btn
+    // adt.addControl(line)
+
+    // line.y2 = 0
+    // line.linkOffsetY = 0
+    // line.linkOffsetX = 0
+    // line.dash = [3, 3]
+
+    adt.addControl(btn)
+
+    return btn
   }
 }
 
